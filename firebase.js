a88c1262ed2428b310e1f787244cf166d3cc8530
Thế Nghĩa <nghiask2018@gmail.com>
2023-12-08 00:28:53 +0700

    Demo read real-time data

---
+++ firebase.js
@@ -0,0 +1,24 @@
+
+// ======= Import the functions
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database"; // Realtime Database
+
+// ======= Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD7Hl6psEa09ktEpUcpAULhChv4pD8M6IU",
+    authDomain: "joyfeed-6f7a8.firebaseapp.com",
+    projectId: "joyfeed-6f7a8",
+    storageBucket: "joyfeed-6f7a8.appspot.com",
+    messagingSenderId: "79904275884",
+    appId: "1:79904275884:web:71591f6e81392cc4e9ce09",
+    measurementId: "G-J1KKL13YBP",
+    databaseURL: "https://joyfeed-6f7a8-default-rtdb.asia-southeast1.firebasedatabase.app/"
+};
+
+// ======= Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// ======= Initialize Realtime Database
+const db = getDatabase(app);
+
+export default db;
--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,42 @@
 
-import './App.css'
+// Import functions
+import { useState, useEffect } from 'react';
+import { ref, onValue } from 'firebase/database';
+import db from '../firebase';
+
 
 const App = () => {
+    // ======= Init States
+    const [temperature, setTemperature] = useState(0);
+    const [humidity, setHumidity] = useState(0);
+
+    // ======= Read real-time data (Chỉ chạy một lần sau khi component mount)
+    useEffect(() => {
+        const temperatureRef = ref(db, 'temperature');
+        const humidityRef = ref(db, 'humidity');
+
+        // Đọc Nhiệt độ
+        onValue(temperatureRef, (snapshot) => {
+            setTemperature(snapshot.val());
+        });
+
+        // Đọc Độ ẩm
+        onValue(humidityRef, (snapshot) => {
+            setHumidity(snapshot.val());
+        });
+
+        // Cleanup function khi component unmount
+        return () => {
+            off(temperatureRef); // Remove event listeners để tránh memory leaks khi component unmount
+            off(humidityRef);
+        };
+    }, []);
+
     return (
-        <div>
+        <div className='main'>
             <h1>Hello world</h1>
+            <h1>Temperature : {temperature}</h1>
+            <h1>Humidity : {humidity}</h1>
         </div>
     )
 }
