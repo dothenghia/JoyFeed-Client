fec0a407e10c4def58538df40b88f28f1be15a71
Thế Nghĩa <nghiask2018@gmail.com>
2023-12-12 15:01:27 +0700

    Setup package TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'primary': '#0D6EFD',
+                'primary-subtle': '#75ADFF',
+                'primary-light': '#CFE2FF',
+
+                'border': '#DEE2E6',
+
+                'title': '#212529',
+                'subtitle': '#878787',
+                'text': '#444444',
+            },
+        },
+    },
+    plugins: [],
+}
+
