ab0aa6f640240662db1759ca5eab447aff06027e
Thế Nghĩa <nghiask2018@gmail.com>
2023-12-13 20:58:59 +0700

    Fix bug Config Screen

--- src/screens/Config.jsx
+++ src/screens/Config.jsx
@@ -1,6 +1,26 @@
 
+import { useState } from "react"
 
 const Config = () => {
+    // ========== Init state ==========
+    const [meals, setMeals] = useState(3)
+    const [sound, setSound] = useState("Tiếng gọi hoang dã")
+
+
+    // ========== Handle functions ==========
+    function handleDecreaseMeal() {
+        if (meals === 1) return
+        setMeals(meals-1)
+    }
+    function handleIncreaseMeal() {
+        if (meals === 5) return
+        setMeals(meals+1)
+    }
+    function handleSoundChange(e) {
+        setSound(e.target.value)
+    }
+
+
     return (
         <div className="max-w-3xl mx-auto px-5 sm:px-10 h-full pt-6">
 
@@ -9,20 +29,21 @@ const Config = () => {
                     <label htmlFor="meals" className="form-label">Số bữa ăn</label>
 
                     <div className="flex mb-4 sm:mb-0">
-                        <button className="custom-primary-fade-btn px-2"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-minus"><line x1="5" y1="12" x2="19" y2="12"></line></svg></button>
+                        <button className="custom-primary-fade-btn px-2" onClick={handleDecreaseMeal}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-minus"><line x1="5" y1="12" x2="19" y2="12"></line></svg></button>
                         <input
                             type="number"
                             id="meals"
                             className="form-input m-0 w-14 text-center mx-2"
-                            value={3}
+                            value={meals}
+                            onChange={e => setMeals(e.target.value)}
                         />
-                        <button className="custom-primary-fade-btn px-2"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-plus"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg></button>
+                        <button className="custom-primary-fade-btn px-2" onClick={handleIncreaseMeal}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-plus"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg></button>
                     </div>
                 </div>
 
                 <div className="w-fit">
                     <label htmlFor="sound" className="form-label">Âm thanh</label>
-                    <select id="sound" className="form-input m-0">
+                    <select id="sound" className="form-input m-0" onChange={handleSoundChange}>
                         <option value="Tiếng gọi hoang dã">Tiếng gọi hoang dã</option>
                         <option value="Sao em lại không nói">Sao em lại không nói</option>
                         <option value="Tìm em trong bóng damn">Tìm em trong bóng damn</option>
@@ -31,23 +52,27 @@ const Config = () => {
             </div>
 
             <div>
-                <div className="px-4 pt-3 pb-2 border border-1 border-border rounded-xl overflow-hidden">
-                    <h1 className="text-lg font-semibold mb-2">Lần 1</h1>
+                {
+                    [...Array(meals)].map((_, index) => (
+                        <div key={index} className="px-4 pt-3 pb-2 border border-1 border-border rounded-xl overflow-hidden mb-5">
+                            <h1 className="text-lg font-semibold mb-2">Lần {index + 1}</h1>
 
-                    <div>
-                        <label htmlFor="sound" className="form-label-inline">Thời gian: </label>
-                        <input type="time" className="form-input-inline" />
-                    </div>
+                            <div>
+                                <label htmlFor="sound" className="form-label-inline">Thời gian: </label>
+                                <input type="time" className="form-input-inline" />
+                            </div>
 
-                    <div>
-                        <label htmlFor="sound" className="form-label-inline">Khối lượng: </label>
-                        <input type="number" className="form-input-inline w-28" />
-                        <span className="text-base font-medium text-text ml-2">gram</span>
-                    </div>
-                </div>
+                            <div>
+                                <label htmlFor="sound" className="form-label-inline">Khối lượng: </label>
+                                <input type="number" className="form-input-inline w-28" />
+                                <span className="text-base font-medium text-text ml-2">gram</span>
+                            </div>
+                        </div>
+                    ))
+                }
             </div>
 
-            <div className="mt-5 flex justify-end">
+            <div className="flex justify-end pb-8">
                 <button type="submit" className="custom-primary-btn">Lưu thông tin</button>
             </div>
         </div>
