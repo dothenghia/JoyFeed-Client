4d8f7173f16be6db452189c29403022f8f48443a
caokhoi2702 <ngcaokhoi2702@gmail.com>
2023-12-20 20:12:38 +0700

    cleaning esp code

--- ESP/ref_khoi.ino
+++ ESP/ref_khoi.ino
@@ -20,8 +20,8 @@
 #define FIREBASE_AUTH "3INg6iazTAVbOqB9NUC8ke9HY1C4yGAnBJIm5gV9"
 #define WIFI_SSID "top 1 si tinh iu em ko loi thoat"
 #define WIFI_PASSWORD "123456789@"
-#define ESP_SSID "joyfeed_wifi"
-#define ESP_PASSWORD "123456789"
+// #define ESP_SSID "joyfeed_wifi"
+// #define ESP_PASSWORD "123456789"
 
 // Initializating for World Time
 const long utcOffsetInSeconds = 7 * 3600;
@@ -37,17 +37,17 @@ String path = "1234/";
 String ssid = "";
 String password = "";
 
-const char index_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE HTML><html><head>
-  <title>ESP Input Form</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  </head><body>
-  <form action="/get">
-    wifi's name: <input type="text" name="wifi's name"></br>
-    wifi's password: <input type="text" name="wifi's password"></br>
-    <input type="submit" value="Submit">
-  </form><br>
-</body></html>)rawliteral";
+// const char index_html[] PROGMEM = R"rawliteral(
+// <!DOCTYPE HTML><html><head>
+//   <title>ESP Input Form</title>
+//   <meta name="viewport" content="width=device-width, initial-scale=1">
+//   </head><body>
+//   <form action="/get">
+//     wifi's name: <input type="text" name="wifi's name"></br>
+//     wifi's password: <input type="text" name="wifi's password"></br>
+//     <input type="submit" value="Submit">
+//   </form><br>
+// </body></html>)rawliteral";
 
 
 // HX711 and Loadcell initialization
@@ -69,7 +69,7 @@ const int in2 = 2;
 const int buzzer = 5;
 
 // time limit for notification
-float time_no_food = -1000*3600*12; //12 hours -> milliseconds
+float time_no_food = -1000*3600*12; // 12 hours -> milliseconds
 float H12 = 1000*3600*12;
 float limit_distance = 16.5;
 
@@ -97,37 +97,37 @@ void connect2wifi()
   Serial.println("Connected.");
 }
 
-void setupAccessPoint()
-{
-  // Setup ESP as a Access Point and setup ESP to connect to an Access Point
-  WiFi.softAP(ESP_SSID, ESP_PASSWORD);
-  IPAddress apIP(192, 168, 1, 84);
-  IPAddress subnet(255, 255, 255, 0);
-  WiFi.softAPConfig(apIP, apIP, subnet);
+// void setupAccessPoint()
+// {
+//   // Setup ESP as a Access Point and setup ESP to connect to an Access Point
+//   WiFi.softAP(ESP_SSID, ESP_PASSWORD);
+//   IPAddress apIP(192, 168, 1, 84);
+//   IPAddress subnet(255, 255, 255, 0);
+//   WiFi.softAPConfig(apIP, apIP, subnet);
 
-  Serial.print("ESP32 IP as soft AP: ");
-  Serial.println(WiFi.softAPIP());
+//   Serial.print("ESP32 IP as soft AP: ");
+//   Serial.println(WiFi.softAPIP());
  
-  Serial.print("ESP32 IP on the WiFi network: ");
-  Serial.println(WiFi.localIP());
+//   Serial.print("ESP32 IP on the WiFi network: ");
+//   Serial.println(WiFi.localIP());
 
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/html", index_html);
-  });
+//   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+//     request->send_P(200, "text/html", index_html);
+//   });
 
-  // Send a GET request to <ESP_IP>/get?input1=<inputMessage>
-  server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
-    String inputParam;
-    String inputMessage;
-    // GET input1 value on <ESP_IP>/get?input1=<inputMessage>
-    ssid = request->getParam("wifi's name")->value();
-    password = request->getParam("wifi's password")->value();
-    request->send(200, "text/html", "HTTP GET request sent to your ESP on input field (with value: " + ssid + "," + password +
-                                     "<br><a href=\"/\">Return to Home Page</a>");
-  });
+//   // Send a GET request to <ESP_IP>/get?input1=<inputMessage>
+//   server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
+//     String inputParam;
+//     String inputMessage;
+//     // GET input1 value on <ESP_IP>/get?input1=<inputMessage>
+//     ssid = request->getParam("wifi's name")->value();
+//     password = request->getParam("wifi's password")->value();
+//     request->send(200, "text/html", "HTTP GET request sent to your ESP on input field (with value: " + ssid + "," + password +
+//                                      "<br><a href=\"/\">Return to Home Page</a>");
+//   });
 
-  server.begin();
-}
+//   server.begin();
+// }
 
 float getWeight()
 {
@@ -136,27 +136,27 @@ float getWeight()
   return weight > 0 ? weight : 0;
 }
 
-void runMotor(float time)
-{
-  long long curTime = millis();
-  while(millis()-curTime < 300)
-  {
-    analogWrite(ena,150);
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, HIGH);
-  }
-  delay(50);
-  while (millis() - curTime < time * 1000)
-  {
-    Serial.println("Start motor");
-    analogWrite(ena,70);
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, HIGH);
-  }
-  analogWrite(ena, 0);
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, LOW);
-}
+// void runMotor(float time)
+// {
+//   long long curTime = millis();
+//   while(millis() - curTime < 300)
+//   {
+//     analogWrite(ena,150);
+//     digitalWrite(in1, LOW);
+//     digitalWrite(in2, HIGH);
+//   }
+//   delay(50);
+//   while (millis() - curTime < time * 1000)
+//   {
+//     Serial.println("Start motor");
+//     analogWrite(ena,70);
+//     digitalWrite(in1, LOW);
+//     digitalWrite(in2, HIGH);
+//   }
+//   analogWrite(ena, 0);
+//   digitalWrite(in1, LOW);
+//   digitalWrite(in2, LOW);
+// }
 
 void feed(float gram)
 {
@@ -242,23 +242,12 @@ void alert(int index)
   else if (index == 2)
   {
     int notes[] = {
-      NOTE_B4, NOTE_B5, NOTE_FS5, NOTE_DS5,
-      NOTE_B5, NOTE_FS5, NOTE_DS5, NOTE_C5,
-      NOTE_C6, NOTE_G6, NOTE_E6, NOTE_C6, NOTE_G6, NOTE_E6,
-      
-      NOTE_B4, NOTE_B5, NOTE_FS5, NOTE_DS5, NOTE_B5,
-      NOTE_FS5, NOTE_DS5, NOTE_DS5, NOTE_E5, NOTE_F5,
-      NOTE_F5, NOTE_FS5, NOTE_G5, NOTE_G5, NOTE_GS5, NOTE_A5, NOTE_B5
+      NOTE_B4, NOTE_B5, NOTE_FS5, NOTE_DS5, NOTE_B5, NOTE_FS5, NOTE_DS5, NOTE_C5, NOTE_C6, NOTE_G6, NOTE_E6, NOTE_C6, NOTE_G6, NOTE_E6,
+      NOTE_B4, NOTE_B5, NOTE_FS5, NOTE_DS5, NOTE_B5, NOTE_FS5, NOTE_DS5, NOTE_DS5, NOTE_E5, NOTE_F5, NOTE_F5, NOTE_FS5, NOTE_G5, NOTE_G5, NOTE_GS5, NOTE_A5, NOTE_B5
     };
 
     int durations[] = {
-      16, 16, 16, 16,
-      32, 16, 8, 16,
-      16, 16, 16, 32, 16, 8,
-      
-      16, 16, 16, 16, 32,
-      16, 8, 32, 32, 32,
-      32, 32, 32, 32, 32, 16, 8
+      16, 16, 16, 16, 32, 16, 8, 16, 16, 16, 16, 32, 16, 8, 16, 16, 16, 16, 32, 16, 8, 32, 32, 32, 32, 32, 32, 32, 32, 16, 8
     };
 
     const int totalNotes = sizeof(notes) / sizeof(int);
@@ -272,7 +261,6 @@ void alert(int index)
       noTone(buzzer);
     }
   }
-  // tone(buzzer, 263, 1000);
 }
 
 String getWorldTime()
